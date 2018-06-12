468051eda6a11727058d1c1f44cbc5c7db20fb64
zsc <1240386795@qq.com>
2018-06-12 15:39:38 +0800

    2 commit

--- index.php
+++ index.php
@@ -2,3 +2,6 @@
 echo "Git is a distributed version control system.
 Git is free software.";
 echo phpinfo();
+
+Git is a distributed version control system.
+Git is free software distributed under the GPL.
