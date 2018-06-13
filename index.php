4f6f5073d0b0ea44fd1afb49166fc534ed7c09ac
zsc <1240386795@qq.com>
2018-06-13 08:43:16 +0800

    xiugaiadd xiugai commit

--- index.php
+++ index.php
@@ -3,5 +3,3 @@ echo "Git is a distributed version control system.
 Git is free software.";
 echo phpinfo();
 
-Git is a distributed version control system.
-Git is free software distributed under the GPL.
